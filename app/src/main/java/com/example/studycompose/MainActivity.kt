74cee8bda51741a606480babd3b262212a76e902
sisosyunya <syunyasisotto@gmail.com>
2023-05-01 05:45:23 +0900

    [add]todolist

--- app/src/main/java/com/example/studycompose/MainActivity.kt
+++ app/src/main/java/com/example/studycompose/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.studycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.ModifierLocalReadScope
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.studycompose.ui.theme.StudyComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +23,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StudyComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    val todoList =(1..100).map { "タスク${it}" }
+                    MainScreen()
                 }
             }
         }
@@ -30,14 +33,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainScreen() {
+    var todoList:SnapshotStateList<String> by remember { mutableStateOf(mutableStateListOf()) }
+    var text: String by remember { mutableStateOf("") }
+    Column {
+        CenterAlignedTopAppBar(
+            title = { Text("Todo List") }
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ){
+            items(todoList){todo ->
+                Text(text = todo,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp))
+            }
+        }
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)){
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+            label = { Text("todo")},
+            modifier = Modifier.wrapContentHeight().weight(1f))
+            Spacer(Modifier.size(16.dp))
+            Button(onClick = {
+                if (text.isEmpty()) return@Button
+                todoList.add(text)
+                text=""
+            },
+            modifier = Modifier.align(Alignment.CenterVertically)) {
+                Text("add")
+            }
+        }
+    }
+
+    }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     StudyComposeTheme {
-        Greeting("Android")
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            val todoList = (1..100).map { "タスク${it}" }
+            MainScreen()
+        }
     }
 }
